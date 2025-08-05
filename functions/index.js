3017c2a76cf05f24194688e665aa4a40a702e761
sassongal <sassong4l@gmail.com>
2025-08-05 05:13:37 +0300

    feat: Add WhatsApp bot function and dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,49 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-// Import functions when needed
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const axios = require("axios");
+const twilio = require("twilio");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const twilioAccountSid = functions.config().twilio.account_sid;
+const twilioAuthToken = functions.config().twilio.auth_token;
+const twilioPhoneNumber = functions.config().twilio.phone_number;
+const googleApiKey = functions.config().google.api_key;
+
+const twilioClient = twilio(twilioAccountSid, twilioAuthToken);
+
+exports.whatsappBot = functions.https.onRequest(async (req, res) => {
+  const incomingMsg = req.body.Body;
+  const from = req.body.From;
+
+  try {
+    const response = await axios.post(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${googleApiKey}`,
+        {
+          contents: [
+            {
+              parts: [
+                {
+                  text: incomingMsg,
+                },
+              ],
+            },
+          ],
+        },
+    );
+
+    const botResponse = response.data.candidates[0].content.parts[0].text;
+
+    await twilioClient.messages.create({
+      body: botResponse,
+      from: twilioPhoneNumber,
+      to: from,
+    });
+
+    res.status(200).send("Message sent successfully!");
+  } catch (error) {
+    console.error("Error processing message:", error);
+    res.status(500).send("Error processing message");
+  }
+});
